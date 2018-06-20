f22282fea6dcd0fe8cdd2c3060dc8df054c4fa3b
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-06-20 17:38:27 +0800

    Eliminate unnecessary http request (#16)
    * use static final field to hold this data.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/springcloudplayground/autoconfigure/DefaultGeneratorMetadataProvider.java
+++ src/main/java/com/microsoft/azure/springcloudplayground/autoconfigure/DefaultGeneratorMetadataProvider.java
@@ -4,18 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.springcloudplayground.metadata.DefaultMetadataElement;
 import com.microsoft.azure.springcloudplayground.metadata.GeneratorMetadata;
 import com.microsoft.azure.springcloudplayground.metadata.GeneratorMetadataProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DefaultGeneratorMetadataProvider implements GeneratorMetadataProvider {
 
-    private static final Logger log = LoggerFactory
-            .getLogger(DefaultGeneratorMetadataProvider.class);
+    private static final List<DefaultMetadataElement> bootVersions = Arrays.asList(
+            DefaultMetadataElement.create("2.1.0.BUILD-SNAPSHOT", "2.1.0 (SNAPSHOT)", false),
+            DefaultMetadataElement.create("2.0.4.BUILD-SNAPSHOT", "2.0.4 (SNAPSHOT)", false),
+            DefaultMetadataElement.create("2.0.3.RELEASE", "2.0.3", true),
+            DefaultMetadataElement.create("1.5.15.BUILD-SNAPSHOT", "1.5.15 (SNAPSHOT)", false),
+            DefaultMetadataElement.create("1.5.14.RELEASE", "1.5.14", false)
+    );
 
     private final GeneratorMetadata metadata;
 
@@ -38,28 +41,26 @@ public class DefaultGeneratorMetadataProvider implements GeneratorMetadataProvid
     }
 
     protected void updateGeneratorMetadata(GeneratorMetadata metadata) {
-        List<DefaultMetadataElement> bootVersions = fetchBootVersions();
-        if (bootVersions != null && !bootVersions.isEmpty()) {
-            if (bootVersions.stream().noneMatch(DefaultMetadataElement::isDefault)) {
-                // No default specified
-                bootVersions.get(0).setDefault(true);
-            }
-            metadata.updateSpringBootVersions(bootVersions);
+        if (bootVersions.stream().noneMatch(DefaultMetadataElement::isDefault)) {
+            // No default specified
+            bootVersions.get(0).setDefault(true);
         }
+
+        metadata.updateSpringBootVersions(bootVersions);
     }
 
-    protected List<DefaultMetadataElement> fetchBootVersions() {
-        String url = this.metadata.getConfiguration().getEnv().getSpringBootMetadataUrl();
-        if (StringUtils.hasText(url)) {
-            try {
-                log.info("Fetching boot metadata from {}", url);
-                return new SpringBootMetadataReader(this.objectMapper, this.restTemplate, url).getBootVersions();
-            }
-            catch (Exception e) {
-                log.warn("Failed to fetch spring boot metadata", e);
-            }
-        }
-        return null;
-    }
-
+// Keep following method in case the data changes.
+//    protected List<DefaultMetadataElement> fetchBootVersions() {
+//        String url = this.metadata.getConfiguration().getEnv().getSpringBootMetadataUrl();
+//        if (StringUtils.hasText(url)) {
+//            try {
+//                log.info("Fetching boot metadata from {}", url);
+//                return new SpringBootMetadataReader(this.objectMapper, this.restTemplate, url).getBootVersions();
+//            }
+//            catch (Exception e) {
+//                log.warn("Failed to fetch spring boot metadata", e);
+//            }
+//        }
+//        return null;
+//    }
 }
