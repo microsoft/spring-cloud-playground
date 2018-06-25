68197c13c9cc7dbb94a7d899023031233d998b92
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2018-06-25 10:50:45 +0800

    Add expand button for configure port (#19)
    * Add expand link for advanced configuration.
    
    * Add one link for showing/hiding advanced configuration.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/springcloudplayground/service/ServiceModule.java
+++ src/main/java/com/microsoft/azure/springcloudplayground/service/ServiceModule.java
@@ -1,5 +1,6 @@
 package com.microsoft.azure.springcloudplayground.service;
 
+import com.microsoft.azure.springcloudplayground.generator.ServiceMetadata;
 import com.microsoft.azure.springcloudplayground.metadata.Describable;
 import com.microsoft.azure.springcloudplayground.metadata.MetadataElement;
 import lombok.Getter;
@@ -21,7 +22,9 @@ public class ServiceModule extends MetadataElement implements Describable {
     @Getter
     private String portName;
 
-    @Override
+    @Getter
+    private Integer defaultPort;
+
     public String getDescription() {
         return this.description;
     }
@@ -30,6 +33,7 @@ public class ServiceModule extends MetadataElement implements Describable {
         super(id, name);
         this.description = description;
         this.portName = toPortName(id);
+        this.defaultPort = getDefaultPort(id);
     }
 
     private String toPortName(@NonNull String id) {
@@ -44,6 +48,13 @@ public class ServiceModule extends MetadataElement implements Describable {
     public void setId(@NonNull String id) {
         this.id = id;
         this.portName = toPortName(id);
+        this.defaultPort = getDefaultPort(id);
+    }
+
+    private Integer getDefaultPort(@NonNull String id) {
+        final Integer port = ServiceMetadata.portMap.get(id);
+
+        return port == null ? 0 : port;
     }
 
     @Override
