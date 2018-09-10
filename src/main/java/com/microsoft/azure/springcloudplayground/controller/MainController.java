811b1c92d4701309340707eb14ec03d08f9d908d
weiping <wepa@microsoft.com>
2018-09-10 10:46:07 +0800

    remove unused imports and variable

--- src/main/java/com/microsoft/azure/springcloudplayground/controller/MainController.java
+++ src/main/java/com/microsoft/azure/springcloudplayground/controller/MainController.java
@@ -14,11 +14,8 @@ import org.apache.tools.ant.types.ZipFileSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StreamUtils;
@@ -41,9 +38,6 @@ public class MainController extends AbstractPlaygroundController {
 
     private final TelemetryProxy telemetryProxy;
     private final ProjectGenerator projectGenerator;
-
-    @Autowired
-    private OAuth2AuthorizedClientService authorizedClientService;
 
     private static final String TELEMETRY_EVENT_ACCESS = "SpringCloudPlaygroundAccess";
     private static final String TELEMETRY_EVENT_GENERATE = "SpringCloudPlaygroundGenerate";
