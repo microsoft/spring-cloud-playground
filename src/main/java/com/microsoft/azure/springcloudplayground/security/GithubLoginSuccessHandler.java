890c229bfb12c6c2088723c729a98f93bb078bae
Pan Li <panli@microsoft.com>
2018-09-11 17:02:57 +0800

    Add rediection of success handler.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/springcloudplayground/security/GithubLoginSuccessHandler.java
+++ src/main/java/com/microsoft/azure/springcloudplayground/security/GithubLoginSuccessHandler.java
@@ -1,21 +1,28 @@
 package com.microsoft.azure.springcloudplayground.security;
 
 import com.microsoft.azure.springcloudplayground.util.TelemetryProxy;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@NoArgsConstructor
 public class GithubLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private static final String TELEMETRY_EVENT_GITHUB_LOGIN = "playground-github-login";
 
     private final TelemetryProxy telemetryProxy = new TelemetryProxy();
+
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     private void triggerGithubLoginSuccessEvent(@NonNull String username) {
         Map<String, String> properties = new HashMap<>();
@@ -26,10 +33,14 @@ public class GithubLoginSuccessHandler implements AuthenticationSuccessHandler {
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth)
+            throws IOException {
+        String targetUrl = "/";
         OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) auth;
         String username = oauthToken.getPrincipal().getAttributes().get("login").toString();
 
-        this.triggerGithubLoginSuccessEvent(username);
+        triggerGithubLoginSuccessEvent(username);
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 }
